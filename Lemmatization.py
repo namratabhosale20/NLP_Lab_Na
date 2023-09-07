1656918c9f039a1d112b73b367e2391c8fcc6547
Namrata Bhosale <104768013+namratabhosale20@users.noreply.github.com>
2023-09-07 15:49:18 +0530

    Add files via upload

--- Lemmatization.py
+++ Lemmatization.py
@@ -1,19 +1,32 @@
-#Assignment No-01
-#Name:-Namrata Bhosale
-#Roll no:-08, Batch:-B1
-#Title:-Lemmatization
+# Assignment No-01
+# Name:-Namrata Bhosale
+# Roll no:-08, Batch:-B1
+# Title:-Lemmatization
 
-#Import Library
+# Import Library
 import spacy
+
 nlp = spacy.load("en_core_web_sm")
 conference_help_text = (
-   "Robert was a good king."
-     " He wanted to bring peace to his kingdom."
-     " There were many others who wanted to become king."
-     "They started plotting against him."
- )
-#Lemmatization
+    "Robert was a good king."
+    " He wanted to bring peace to his kingdom."
+    " There were many others who wanted to become king."
+    "They started plotting against him."
+)
+# Lemmatization
 conference_help_doc = nlp(conference_help_text)
 for token in conference_help_doc:
-     if str(token) != str(token.lemma_):
-          print(f"{str(token):>20} : {str(token.lemma_)}")
+    if str(token) != str(token.lemma_):
+        print(f"{str(token):>20} : {str(token.lemma_)}")
+# OUTPUT:-
+#  was : be
+#  He : he
+#  wanted : want
+#   There : there
+#  were : be
+#  others : other
+#  wanted : want
+#  They : they
+#  started : start
+#  plotting : plot
+#  him : he
